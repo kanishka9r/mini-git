63d72b6d5fdbb3aa5680dcfccb06953c786e20ff
kanishka9r <kanishkarajput4918@gmail.com>
2026-01-12 00:47:06 +0530

    modified:   include/vcs.h 	modified:   src/main.cpp

---
+++ include/vcs.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+class VCS {
+public:
+    static void init();
+    static void add(const string& filename);
+    static void commit(const string& message);
+    static void branch(const string& name);
+    static void checkout(const string& name);
+    static void log();
+};
---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cout << "No command provided\n";
+        return 0;
+    }
+
+    string cmd = argv[1];
+
+    if (cmd == "init") {
+        // will call VCS::init()
+    }
+    else if (cmd == "add") {
+        // will call VCS::add()
+    }
+    else if (cmd == "commit") {
+        // will call VCS::commit()
+    }
+    else if (cmd == "branch") {
+        // will call VCS::branch()
+    }
+    else if (cmd == "checkout") {
+        // will call VCS::checkout()
+    }
+    else if (cmd == "log") {
+        // will call VCS::log()
+    }
+    else if (cmd == "diff") {
+        // will call Diff::run()
+    }
+    else if (cmd == "merge") {
+        // will call Merge::run()
+    }
+    else {
+        cout << "Unknown command\n";
+    }
+
+    return 0;
+}
