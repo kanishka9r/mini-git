cbdcb3c5c072f08c86164c84de094d8b738019db
kanishka9r <kanishkarajput4918@gmail.com>
2026-05-27 18:11:24 +0530

    lcs added

--- src/diff.cpp
+++ src/diff.cpp
@@ -1,5 +1,5 @@
 #include "diff.h"
-
+#include <algorithm>
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -37,6 +37,99 @@ void Diff::run(
         lines2.push_back(line);
     }
 
-    cout << "File 1 lines: " << lines1.size() << endl;
-    cout << "File 2 lines: " << lines2.size() << endl;
+    int n = lines1.size();
+    int m = lines2.size();
+
+    vector<vector<int>> dp(
+    n + 1,
+    vector<int>(m + 1, 0)
+    );
+
+   for (int i = 1; i <= n; i++)
+   {
+      for (int j = 1; j <= m; j++)
+       {
+        // same line
+        if (lines1[i - 1] == lines2[j - 1])
+        {
+            dp[i][j] =
+                1 + dp[i - 1][j - 1];
+        }
+
+        // different lines
+        else
+        {
+            dp[i][j] = max(
+                dp[i - 1][j],
+                dp[i][j - 1]
+            );
+        }
+        }
+    }
+    cout << "LCS Length = "<< dp[n][m]<< endl;
+    vector<string> diffOutput;
+
+int i = n;
+int j = m;
+
+while (i > 0 && j > 0)
+{
+    // same line
+    if (lines1[i - 1] == lines2[j - 1])
+    {
+        i--;
+        j--;
+    }
+
+    // line removed
+    else if (dp[i - 1][j] > dp[i][j - 1])
+    {
+        diffOutput.push_back(
+            "- " + lines1[i - 1]
+        );
+
+        i--;
+    }
+
+    // line added
+    else
+    {
+        diffOutput.push_back(
+            "+ " + lines2[j - 1]
+        );
+
+        j--;
+    }
+}
+// remaining old lines
+while (i > 0)
+{
+    diffOutput.push_back(
+        "- " + lines1[i - 1]
+    );
+
+    i--;
+}
+
+// remaining new lines
+while (j > 0)
+{
+    diffOutput.push_back(
+        "+ " + lines2[j - 1]
+    );
+
+    j--;
+}
+
+reverse(
+    diffOutput.begin(),
+    diffOutput.end()
+);
+
+for (const string& line : diffOutput)
+{
+    cout << line << endl;
+}
+
+
 }
