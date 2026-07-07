3275a1da16097e6366170508077579cf08481f39
kanishka9r <kanishkarajput4918@gmail.com>
2026-07-07 20:42:41 +0530

    optimized lcs

--- src/diff.cpp
+++ src/diff.cpp
@@ -38,101 +38,83 @@ void Diff::run(
         lines2.push_back(line);
     }
 
-    int n = lines1.size();
-    int m = lines2.size();
+    int prefixCount = 0;
+    while (prefixCount < lines1.size() && prefixCount < lines2.size() && 
+           lines1[prefixCount] == lines2[prefixCount]) {
+        prefixCount++;
+    }
 
-    vector<vector<int>> dp(
-    n + 1,
-    vector<int>(m + 1, 0)
-    );
+    int suffixCount = 0;
+    while (suffixCount < (lines1.size() - prefixCount) && 
+           suffixCount < (lines2.size() - prefixCount) && 
+           lines1[lines1.size() - 1 - suffixCount] == lines2[lines2.size() - 1 - suffixCount]) {
+        suffixCount++;
+    }
 
-   for (int i = 1; i <= n; i++)
-   {
-      for (int j = 1; j <= m; j++)
-       {
-        // same line
-        if (lines1[i - 1] == lines2[j - 1])
+    int n = lines1.size() - prefixCount - suffixCount;
+    int m = lines2.size() - prefixCount - suffixCount;
+
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
         {
-            dp[i][j] =
-                1 + dp[i - 1][j - 1];
-        }
-
-        // different lines
-        else
-        {
-            dp[i][j] = max(
-                dp[i - 1][j],
-                dp[i][j - 1]
-            );
-        }
+            if (lines1[prefixCount + i - 1] == lines2[prefixCount + j - 1])
+            {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            }
+            else
+            {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
         }
     }
-    cout << "LCS Length = "<< dp[n][m]<< endl;
+    
+    int totalLcs = prefixCount + suffixCount + dp[n][m];
+    cout << "LCS Length = "<< totalLcs << endl;
     vector<string> diffOutput;
 
-int i = n;
-int j = m;
+    int i = n;
+    int j = m;
 
-while (i > 0 && j > 0)
-{
-    // same line
-    if (lines1[i - 1] == lines2[j - 1])
+    while (i > 0 && j > 0)
     {
+        if (lines1[prefixCount + i - 1] == lines2[prefixCount + j - 1])
+        {
+            i--;
+            j--;
+        }
+        else if (dp[i - 1][j] > dp[i][j - 1])
+        {
+            diffOutput.push_back("- " + lines1[prefixCount + i - 1]);
+            i--;
+        }
+        else
+        {
+            diffOutput.push_back("+ " + lines2[prefixCount + j - 1]);
+            j--;
+        }
+    }
+    
+    while (i > 0)
+    {
+        diffOutput.push_back("- " + lines1[prefixCount + i - 1]);
         i--;
+    }
+
+    while (j > 0)
+    {
+        diffOutput.push_back("+ " + lines2[prefixCount + j - 1]);
         j--;
     }
 
-    // line removed
-    else if (dp[i - 1][j] > dp[i][j - 1])
+    reverse(diffOutput.begin(), diffOutput.end());
+
+    for (const string& line : diffOutput)
     {
-        diffOutput.push_back(
-            "- " + lines1[i - 1]
-        );
-
-        i--;
+        cout << line << endl;
     }
-
-    // line added
-    else
-    {
-        diffOutput.push_back(
-            "+ " + lines2[j - 1]
-        );
-
-        j--;
-    }
-}
-// remaining old lines
-while (i > 0)
-{
-    diffOutput.push_back(
-        "- " + lines1[i - 1]
-    );
-
-    i--;
-}
-
-// remaining new lines
-while (j > 0)
-{
-    diffOutput.push_back(
-        "+ " + lines2[j - 1]
-    );
-
-    j--;
-}
-
-reverse(
-    diffOutput.begin(),
-    diffOutput.end()
-);
-
-for (const string& line : diffOutput)
-{
-    cout << line << endl;
-}
-
-
 }
 
 //  GUI-facing compute diff from content strings 
@@ -151,56 +133,78 @@ vector<DiffLine> Diff::compute(const string& content1, const string& content2)
     while (getline(s2, line))
         lines2.push_back(line);
 
-    int n = lines1.size();
-    int m = lines2.size();
+    int prefixCount = 0;
+    while (prefixCount < lines1.size() && prefixCount < lines2.size() && 
+           lines1[prefixCount] == lines2[prefixCount]) {
+        prefixCount++;
+    }
 
-    // LCS DP table (same algorithm as run())
+    int suffixCount = 0;
+    while (suffixCount < (lines1.size() - prefixCount) && 
+           suffixCount < (lines2.size() - prefixCount) && 
+           lines1[lines1.size() - 1 - suffixCount] == lines2[lines2.size() - 1 - suffixCount]) {
+        suffixCount++;
+    }
+
+    int n = lines1.size() - prefixCount - suffixCount;
+    int m = lines2.size() - prefixCount - suffixCount;
+
     vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
 
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= m; j++)
         {
-            if (lines1[i - 1] == lines2[j - 1])
+            if (lines1[prefixCount + i - 1] == lines2[prefixCount + j - 1])
                 dp[i][j] = 1 + dp[i - 1][j - 1];
             else
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
         }
     }
 
-    // Backtrack to produce diff
     vector<DiffLine> diffOutput;
-    int i = n, j = m;
+    
+    // 1. Push suffix in reverse
+    for (int k = 0; k < suffixCount; k++) {
+        diffOutput.push_back({' ', lines1[lines1.size() - 1 - k]});
+    }
 
+    // 2. Backtrack middle
+    int i = n, j = m;
     while (i > 0 && j > 0)
     {
-        if (lines1[i - 1] == lines2[j - 1])
+        if (lines1[prefixCount + i - 1] == lines2[prefixCount + j - 1])
         {
-            diffOutput.push_back({' ', lines1[i - 1]});
+            diffOutput.push_back({' ', lines1[prefixCount + i - 1]});
             i--; j--;
         }
         else if (dp[i - 1][j] > dp[i][j - 1])
         {
-            diffOutput.push_back({'-', lines1[i - 1]});
+            diffOutput.push_back({'-', lines1[prefixCount + i - 1]});
             i--;
         }
         else
         {
-            diffOutput.push_back({'+', lines2[j - 1]});
+            diffOutput.push_back({'+', lines2[prefixCount + j - 1]});
             j--;
         }
     }
 
     while (i > 0)
     {
-        diffOutput.push_back({'-', lines1[i - 1]});
+        diffOutput.push_back({'-', lines1[prefixCount + i - 1]});
         i--;
     }
 
     while (j > 0)
     {
-        diffOutput.push_back({'+', lines2[j - 1]});
+        diffOutput.push_back({'+', lines2[prefixCount + j - 1]});
         j--;
+    }
+    
+    // 3. Push prefix in reverse
+    for (int k = prefixCount - 1; k >= 0; k--) {
+        diffOutput.push_back({' ', lines1[k]});
     }
 
     reverse(diffOutput.begin(), diffOutput.end());
