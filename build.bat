efd9c31cd448a9cd7739d21267c60dd48d166ed0
kanishka9r <kanishkarajput4918@gmail.com>
2026-07-15 03:23:22 +0530

    window error

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Compiling Mini-Git using MSYS2 MinGW-w64...
 set PATH=C:\msys64\mingw64\bin;%PATH%
-g++.exe -std=c++17 -Iinclude -Ilibs src\main.cpp src\vcs.cpp src\storage.cpp src\commit.cpp src\branch.cpp src\graph.cpp src\diff.cpp src\config_manager.cpp src\api_server.cpp -o vcs.exe -lws2_32
+g++.exe -std=c++17 -Iinclude -Ilibs src\main.cpp src\vcs.cpp src\storage.cpp src\commit.cpp src\branch.cpp src\graph.cpp src\diff.cpp src\config_manager.cpp src\api_server.cpp -o vcs.exe -lws2_32 -static -static-libgcc -static-libstdc++
 if %errorlevel% equ 0 (
     echo Build successful: vcs.exe
 ) else (
